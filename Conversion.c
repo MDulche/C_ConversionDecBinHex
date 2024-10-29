f594915669ef1b6e1ca4ba86dadf111c58dfd55f
MDulche <mdulche.btsciel24@eleve-irup.com>
2024-10-29 20:15:39 +0100

    Ajout conversion decimal to hexadecimal

--- Conversion.c
+++ Conversion.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int conversionDecToBin(int decimal)
+void conversionDecToBin(int decimal)
 {
     int compteur = 7;
     int binaire[8] = {0,0,0,0,0,0,0,0};
@@ -25,12 +25,59 @@ int conversionDecToBin(int decimal)
         else
         {
             printf("%d", binaire[compteur]);
-        }
-        
-        
+        }  
     }
     printf("]");
     //Affichage du tableau binaire
+}
+
+void conversionDecToHex(int decimal)
+{
+    int compteur = 1;
+    char hexadecimal[2];
+    char comptage[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    int indiceComptage;
+    //Initialisation des variables 
+
+    while (decimal!=0)
+    {
+        indiceComptage = decimal % 16;
+        hexadecimal[compteur] = comptage[indiceComptage];
+        decimal /= 16;
+        compteur--;
+    }
+
+    //Calcul de Décimal à binaire par division successive de 16
+    printf("[");    
+    for (compteur = 0; compteur < 2;compteur++)
+    {
+        if (hexadecimal[compteur]>=0 && hexadecimal[compteur]<=9)
+        {
+            if (compteur != 1)
+            {
+                printf("%d", hexadecimal[compteur]);
+                printf(" ");
+            }
+            else
+            {
+                printf("%d", hexadecimal[compteur]);
+            }
+        }
+        else
+        {
+            if (compteur != 1)
+            {
+                printf("%c", hexadecimal[compteur]);
+                printf(" ");
+            }
+            else
+            {
+                printf("%c", hexadecimal[compteur]);
+            }
+        }
+    }
+    printf("]");  
+    //Affichage du tableau hexadecimal
 }
 
 int main()
@@ -44,6 +91,8 @@ int main()
     //Récupération de la valeur décimal (Base 10) à convertir.
 
     conversionDecToBin(decimal);
-
+    conversionDecToHex(decimal);
     return 0;
+
+    //A faire : Demande de conversion , dec-hex, hex-bin, hex-dec, bin-hex, ip, interface graphique
 }
