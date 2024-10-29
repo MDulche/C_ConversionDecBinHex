8b411c8c0585d5628f4a6162242da369cf433b3e
MDulche <mdulche.btsciel24@eleve-irup.com>
2024-10-29 18:26:48 +0100

    convertisseur dec to bin en fonction

--- Conversion.c
+++ Conversion.c
@@ -2,28 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
-
-int main()
+int conversionDecToBin(int decimal)
 {
-
-    int decimal;
-    int decimalCopie;
-
-    printf("Rentrer votre chiffre à convertir (0-255) : ");
-    scanf("%d", &decimal);
-
-    decimalCopie = decimal;
-
-    //Récupération de la valeur décimal (Base 10) à convertir.
     int compteur = 7;
     int binaire[8] = {0,0,0,0,0,0,0,0};
 
-    while (decimalCopie!=0)
+    while (decimal!=0)
     {
-        binaire[compteur] = decimalCopie % 2;
+        binaire[compteur] = decimal % 2;
         printf("%d\n", binaire[compteur]);
-        decimalCopie /= 2;
-        printf("%d\n", decimalCopie);
+        decimal /= 2;
+        printf("%d\n", decimal);
         compteur--;
     }
 
@@ -31,6 +20,19 @@ int main()
     {
         printf("%d", binaire[compteur]);
     }
+}
+
+int main()
+{
+
+    int decimal;
+
+    printf("Rentrer votre chiffre à convertir (0-255) : ");
+    scanf("%d", &decimal);
+
+    //Récupération de la valeur décimal (Base 10) à convertir.
+
+    conversionDecToBin(decimal);
     
     return 0;
 }
