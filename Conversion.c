aea61a610a95be923260a0f8a61b31f5e7488364
MDulche <mdulche.btsciel24@eleve-irup.com>
2024-10-29 18:23:57 +0100

    Conversion decimal to binaire

--- Conversion.c
+++ Conversion.c
@@ -6,4 +6,31 @@
 int main()
 {
 
+    int decimal;
+    int decimalCopie;
+
+    printf("Rentrer votre chiffre à convertir (0-255) : ");
+    scanf("%d", &decimal);
+
+    decimalCopie = decimal;
+
+    //Récupération de la valeur décimal (Base 10) à convertir.
+    int compteur = 7;
+    int binaire[8] = {0,0,0,0,0,0,0,0};
+
+    while (decimalCopie!=0)
+    {
+        binaire[compteur] = decimalCopie % 2;
+        printf("%d\n", binaire[compteur]);
+        decimalCopie /= 2;
+        printf("%d\n", decimalCopie);
+        compteur--;
+    }
+
+    for (compteur = 0; compteur < 8;compteur++)
+    {
+        printf("%d", binaire[compteur]);
+    }
+    
+    return 0;
 }
